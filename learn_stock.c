891963425747e03149439b48b1d40bfc96490155
Fuchu <93511@GAC.local>
2018-06-06 19:34:30 +0900

    finish task on learn_stock

--- learn_stock.c
+++ learn_stock.c
@@ -11,13 +11,14 @@
 
 #define SETSIZE 100    /* 学習データセットの大きさ */
 #define CNO 10         /* 学習データの桁数 */
-#define GENMAX 10000   /* 解候補 生成回数 */
+#define GENMAX 50000   /* 解候補 生成回数 */
 #define SEED 32767     /* 乱数のシード */
 
 /*****************************************************************************/
 /* proto type definition */
 /*****************************************************************************/
 void read_data(int data[SETSIZE][CNO], int teacher[SETSIZE]);
+void print_data(int data[SETSIZE][CNO], int teacher[SETSIZE]);
 int rand012();
 int calc_score(int data[SETSIZE][CNO], int teacher[SETSIZE], int answer[CNO]);
 
@@ -38,25 +39,34 @@ int main(){
 
     /* 学習データセットの読み込み from stdin */
     read_data(data, teacher);
+    print_data(data, teacher);
 
+    /* START learning */
+    printf("\n");
+    printf("**************** START learning ********************\n");
     for (i=0; i< GENMAX; i++) {
         /* 解候補生成 */
+//        printf("answer[%2d] ", i);
         for (j=0; j < CNO; j++) {
             answer[j] = rand012();
+//            printf("%d ",answer[j]);
         }
+//        printf("\n");
 
         /* check */
         score = calc_score(data, teacher, answer);
 
         /* update best score */
+//        printf("[genno = %3d]:score = %2d  best_score = %2d \n", i, score, best_score);
         if (score > best_score) {
             for (j=0; j < CNO; j++) {
                 best_answer[j] = answer[j];
             }
             best_score = score;
 
+            printf("No %5d:", i);
             for (j=0; j < CNO; j++) {
-                printf("%ld ", best_answer[j]);
+                printf("%d ", best_answer[j]);
             }
             printf(":score=%d\n", best_score);
         }
@@ -93,13 +103,12 @@ int calc_score(int data[SETSIZE][CNO], int teacher[SETSIZE], int answer[CNO]) {
             } else {
                 ;
             }
+        }
 
-            if ( (point == CNO) && (teacher[i] == 0) ){
-                score++;
-            } else if ( (point != CNO) && (teacher[i] == 0) ) {
-                score++;
-            }
-
+        if ( (point == CNO) && (teacher[i] == 1) ){
+            score++;
+        } else if ( (point != CNO) && (teacher[i] == 0) ) {
+            score++;
         }
     }
     return score;
@@ -117,6 +126,22 @@ void read_data(int data[SETSIZE][CNO], int teacher[SETSIZE]) {
         }
         scanf("%d", &teacher[i]);
     }
+}
+/*****************************************************************************/
+/* function print_data() */
+/* 読み込んだ学習データセットを出力 */
+/*****************************************************************************/
+void print_data(int data[SETSIZE][CNO], int teacher[SETSIZE]) {
+    int i, j;
+
+    for (i=0; i < SETSIZE; i++) {
+        printf("No[%2d] ", i);
+        for (j=0; j < CNO; j++) {
+            printf("%d ", data[i][j]);
+        }
+        printf(" --> teacher[%2d]=%d\n", i, teacher[i]);
+    }
+    printf("\n");
 }
 
 /*****************************************************************************/
